75938cfd85691a2ae05fc1ddb1b1eb3e69f9f1e5
Daniel Young <Danyoung91@gmail.com>
2022-11-01 22:52:03 -0400

    commit tree 7ac3dd649919edf09c9bc3acd97a9c4cfafe25df

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+  return Math.abs(42 - block);
+}
+
+function distanceFromHqInFeet(blocks) {
+  return 264 * distanceFromHqInBlocks(blocks);
+}
+
+function distanceTravelledInFeet(first, second) {
+  return 264 * Math.abs(first - second);
+}
+
+function calculatesFarePrice(start, destination) {
+  let route = distanceTravelledInFeet(start, destination);
+
+  if (route > 2500) {
+    return "cannot travel that far";
+  } else if (route > 2000) {
+    return 25;
+  } else if (route > 400) {
+    return 0.02 * (route - 400);
+  } else if (route < 400) {
+    return 0;
+  }
+}
